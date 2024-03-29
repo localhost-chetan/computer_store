958a54718dbc0e8e27114251990ffb0a065efdd2
FlashByteOfficial <flashbyte99@gmail.com>
2024-03-29 18:44:32 +0530

    Modified user.actions.ts

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -4,7 +4,6 @@ import dbConnect from "../mongodb/utils/dbConnect";
 import { type UserType } from "../mongodb/models/User/type/userTypes";
 import User from "../mongodb/models/User/User";
 
-
 const createUser = async (user: UserType) => {
   try {
     await dbConnect();
@@ -12,7 +11,12 @@ const createUser = async (user: UserType) => {
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     if (error instanceof Error) {
-      console.log(`Something went wrong`);
+      if (error.name === "MongoServerError") {
+        console.error("Error creating user:", error.message);
+        // You can also throw a custom error here to propagate it to the calling function
+      } else {
+        console.error("Unexpected error:", error);
+      }
     }
   }
 };
